c3599816d90926adacf7af12011da05ad4495462
vivek3656703 <78290732+vivek3656703@users.noreply.github.com>
2021-07-22 16:34:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import joblib
-model = joblib.load('Sentiment_Analysis01')
+model = joblib.load('Sentiment_Analysis02')
 st.title('Sentiment_Analysis Check')
 ip = st.text_input('Enter your sentiment')
 op = model.predict([ip])
